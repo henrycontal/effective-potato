98f93ce60af21eecca62a7fcb03f2dcc45e1546a
Enrique Contreras <brayancontreras@solemti.com>
2023-05-07 21:44:57 -0700

    ci: render frame

--- src/models/sso.ts
+++ src/models/sso.ts
@@ -15,4 +15,12 @@ export class SSO {
         this.$element = $element;
     }
 
+    render() {
+        const $frame = document.createElement('iframe');
+        const source = 'https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik';
+        $frame.setAttribute('src', source);
+
+        this.$element.append($frame);
+    }
+
 }
