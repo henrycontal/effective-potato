1de1c13a980f1ad0f6e69b6afb088ac74fceff6c
Enrique Contreras <brayancontreras@solemti.mx>
2023-05-17 12:44:46 -0700

    feat: VIVADOT-444

--- scripts/post-changelog/script.ps1
+++ scripts/post-changelog/script.ps1
@@ -6,7 +6,7 @@ $CHANGELOG_PATH="$PWD/$CHANGELOG_FILE"
 Write-Output $CHANGELOG_PATH
 
 # Replace issue numbers with links to the corresponding Jira issue
-(Get-Content $CHANGELOG_PATH) | ForEach-Object {$_ -replace 'VIVADOT-[0-9]+', '[&](https://gruposti.atlassian.net/browse/$&)'} | Set-Content $CHANGELOG_PATH
+(Get-Content $CHANGELOG_PATH) | ForEach-Object {$_ -replace 'VIVADOT-[0-9]+', '[$&](https://gruposti.atlassian.net/browse/$&)'} | Set-Content $CHANGELOG_PATH
 
 # Remove backup file created by sed
 Remove-Item "$CHANGELOG_PATH.bak"
